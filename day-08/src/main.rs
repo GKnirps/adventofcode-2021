ccb4c07c54e2fe5ed9aca2642f1ea7e32f20b928
Guido Knips <guido.knips@posteo.de>
2021-12-08 21:02:53 +0100

    Add solution for day 8, puzzle 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -16,6 +16,12 @@ fn main() -> Result<(), String> {
         trivial_values
     );
 
+    if let Some(number) = sum_numbers(&input) {
+        println!("Sum of numbers is {}", number);
+    } else {
+        println!("Unable to find correct patterns for all values");
+    }
+
     Ok(())
 }
 
@@ -27,15 +33,74 @@ fn count_trivial_values(input: &[(Patterns, Values)]) -> usize {
         .iter()
         .flat_map(|(_, values)| values)
         .filter(|v| {
-            let mut counter: u8 = 0;
-            let mut value: u8 = **v;
-            while value != 0 {
-                counter += value & 1;
-                value >>= 1;
-            }
+            let counter = count_1_bits(**v);
             counter == 2 || counter == 3 || counter == 4 || counter == 7
         })
         .count()
+}
+
+fn count_1_bits(mut value: u8) -> u8 {
+    let mut counter: u8 = 0;
+    while value != 0 {
+        counter += value & 1;
+        value >>= 1;
+    }
+    counter
+}
+
+fn sum_numbers(input: &[(Patterns, Values)]) -> Option<u32> {
+    input
+        .iter()
+        .map(|(patterns, values)| determine_number(patterns, values))
+        .fold(Some(0), |sum, n| Some(sum? + n?))
+}
+
+fn determine_number(patterns: &Patterns, values: &Values) -> Option<u32> {
+    let pattern_1 = patterns.iter().copied().find(|v| count_1_bits(*v) == 2)?;
+    let pattern_7 = patterns.iter().copied().find(|v| count_1_bits(*v) == 3)?;
+    let pattern_4 = patterns.iter().copied().find(|v| count_1_bits(*v) == 4)?;
+    let pattern_8 = patterns.iter().copied().find(|v| count_1_bits(*v) == 7)?;
+    let pattern_3 = patterns
+        .iter()
+        .copied()
+        .find(|v| count_1_bits(*v) == 5 && (*v & pattern_1) == pattern_1)?;
+    let pattern_2 = patterns
+        .iter()
+        .copied()
+        .find(|v| count_1_bits(*v) == 5 && count_1_bits(*v & pattern_4) == 2)?;
+    let pattern_5 = patterns
+        .iter()
+        .copied()
+        .find(|v| *v != pattern_3 && count_1_bits(*v) == 5 && count_1_bits(*v & pattern_4) == 3)?;
+    let pattern_6 = patterns
+        .iter()
+        .copied()
+        .find(|v| count_1_bits(*v) == 6 && count_1_bits(*v & pattern_7) == 2)?;
+    let pattern_9 = patterns
+        .iter()
+        .copied()
+        .find(|v| count_1_bits(*v) == 6 && (*v & pattern_4) == pattern_4)?;
+    let pattern_0 = patterns
+        .iter()
+        .copied()
+        .find(|v| count_1_bits(*v) == 6 && *v != pattern_9 && (*v & pattern_1 == pattern_1))?;
+
+    let ordered_patterns = [
+        pattern_0, pattern_1, pattern_2, pattern_3, pattern_4, pattern_5, pattern_6, pattern_7,
+        pattern_8, pattern_9,
+    ];
+
+    let mut result: u32 = 0;
+    for value in values {
+        result *= 10;
+        result += ordered_patterns
+            .iter()
+            .copied()
+            .enumerate()
+            .find(|(_, p)| p == value)?
+            .0 as u32;
+    }
+    Some(result)
 }
 
 fn parse(input: &str) -> Result<Vec<(Patterns, Values)>, String> {
@@ -125,5 +190,32 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 
         // then
         assert_eq!(count, 26);
+    }
+
+    #[test]
+    fn determine_number_works_for_example() {
+        // given
+        let (patterns, values) = parse_line(
+            "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf",
+        )
+        .expect("expected successful parsing");
+
+        // when
+        let number = determine_number(&patterns, &values);
+
+        // then
+        assert_eq!(number, Some(5353));
+    }
+
+    #[test]
+    fn sum_numbers_works_for_example() {
+        // given
+        let input = parse(EXAMPLE_INPUT).expect("Expected successful parsing");
+
+        // when
+        let sum = sum_numbers(&input);
+
+        // then
+        assert_eq!(sum, Some(61229));
     }
 }
